46ef7be8bb5fdcfc2c6808fcc613ddd5d3471acf
ujjjin2 <ujin7339@naver.com>
2023-01-04 02:11:13 +0900

    스프링부트 day 2

--- src/main/java/studio/thinkground/aroundhub/aroundhub_springboot/controller/HelloControllder.java
+++ src/main/java/studio/thinkground/aroundhub/aroundhub_springboot/controller/HelloControllder.java
@@ -1,12 +1,15 @@
 package studio.thinkground.aroundhub.aroundhub_springboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloControllder {
 
-    @RequestMapping(value = "/hello")
+    //@RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("hello")
     public String hello(){return "Hello, World";}
 
 }
---
+++ src/main/java/studio/thinkground/aroundhub/aroundhub_springboot/dto/MemberDTO.java
@@ -0,0 +1,16 @@
+package studio.thinkground.aroundhub.aroundhub_springboot.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+
+    private String name;
+    private String email;
+    private String organization;
+
+}
